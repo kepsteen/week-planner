b9591773844749c3f2d205f7a63edaf300fb9ed8
Mark Matlock <techmatlock@gmail.com>
2024-06-07 19:25:03 +0000

    lunch on katelyn

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
 var $modal = document.querySelector('dialog');
 var $addNewBtn = document.querySelector('#add-new');
+var $form = document.querySelector('#modal-form');
 if (!$modal)
     throw new Error('no dialog found');
 if (!$addNewBtn)
     throw new Error('no add new button');
+if (!$form)
+    throw new Error('no form');
 $addNewBtn.addEventListener('click', function () {
     $modal.showModal();
+});
+function renderResult(item) {
+    /*
+    <tr>
+        <td>finalalla</td>
+        <td>finalalla</td>
+        <td>finalala</td>
+    </tr>
+  
+    */
+    var $outerElement = document.createElement('tr');
+    var $tdElement1 = document.createElement('td');
+    var $tdElement2 = document.createElement('td');
+    var $tdElement3 = document.createElement('td');
+    $tdElement1.textContent = item.time;
+    $tdElement2.textContent = item.day;
+    $tdElement3.textContent = item.notes;
+    $outerElement.appendChild($tdElement1);
+    $outerElement.appendChild($tdElement2);
+    $outerElement.appendChild($tdElement3);
+}
+$form.addEventListener('submit', function () {
+    // 1. collect the values from the form with the elements property
+    // 2. call the render function with the object
+    // 3. append the render result to the table row
+    var $formElements = $form.elements;
+    var item = {
+        time: $formElements.timeDropdown.value,
+        day: $formElements.weekDropdown.value,
+        notes: $formElements.notesInput.value
+    };
 });
--- main.ts
+++ main.ts
@@ -1,9 +1,61 @@
+interface Item {
+  time: string;
+  day: string;
+  notes: string;
+}
+
+interface FormElements extends HTMLFormControlsCollection{
+  timeDropdown: HTMLSelectElement;
+  weekDropdown: HTMLSelectElement;
+  notesInput: HTMLTextAreaElement;
+}
+
 const $modal = document.querySelector('dialog') as HTMLDialogElement;
 const $addNewBtn = document.querySelector('#add-new') as HTMLAnchorElement;
+const $form = document.querySelector('#modal-form') as HTMLFormElement;
 
 if (!$modal) throw new Error('no dialog found');
 if (!$addNewBtn) throw new Error('no add new button');
+if (!$form) throw new Error('no form');
 
 $addNewBtn.addEventListener('click', () => {
   $modal.showModal();
 });
+
+function renderResult(item: Item): void {
+  /*
+  <tr>
+      <td>finalalla</td>
+      <td>finalalla</td>
+      <td>finalala</td>
+  </tr>
+
+  */
+  const $outerElement = document.createElement('tr');
+  const $tdElement1 = document.createElement('td');
+  const $tdElement2 = document.createElement('td');
+  const $tdElement3 = document.createElement('td');
+
+  $tdElement1.textContent = item.time;
+  $tdElement2.textContent = item.day;
+  $tdElement3.textContent = item.notes;
+
+  $outerElement.appendChild($tdElement1);
+  $outerElement.appendChild($tdElement2);
+  $outerElement.appendChild($tdElement3);
+}
+
+$form.addEventListener('submit', (): void => {
+  // 1. collect the values from the form with the elements property
+  // 2. call the render function with the object
+  // 3. append the render result to the table row
+
+  const $formElements = $form.elements as FormElements;
+
+  const item = {
+    time: $formElements.timeDropdown.value,
+    day: $formElements.weekDropdown.value,
+    notes: $formElements.notesInput.value
+  }
+
+})
