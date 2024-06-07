90e264f6f346f2e88c622239f37a25c5239cafd1
Cody Epstein <codyepstein@gmail.com>
2024-06-07 22:06:07 +0000

    yay we are better than the seniors

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+var eventsObject = {
+    eventsArr: [],
+    editing: null,
+    nextEntryId: 1,
+};
 var $modal = document.querySelector('dialog');
 var $addNewBtn = document.querySelector('#add-new');
 var $form = document.querySelector('#modal-form');
 var $table = document.querySelector('#events-table');
+var $cancelbtn = document.querySelector('#cancel');
 if (!$modal)
     throw new Error('no dialog found');
 if (!$addNewBtn)
@@ -10,28 +16,31 @@ if (!$form)
     throw new Error('no form');
 if (!$table)
     throw new Error('no table');
+if (!$cancelbtn)
+    throw new Error('no cancel button found');
 $addNewBtn.addEventListener('click', function () {
     $modal.showModal();
 });
 function renderResult(item) {
-    /*
-    <tr>
-        <td>finalalla</td>
-        <td>finalalla</td>
-        <td>finalala</td>
-    </tr>
-  
-    */
     var newRow = $table.insertRow(1);
     var newCell1 = newRow.insertCell(0);
     var newCell2 = newRow.insertCell(1);
     var newCell3 = newRow.insertCell(2);
     var $editBtn = document.createElement('a');
     var $deleteBtn = document.createElement('a');
-    $editBtn.setAttribute('class', '');
+    var $plannerActions = document.createElement('div');
+    $editBtn.setAttribute('class', 'edit-btn');
+    $editBtn.setAttribute('href', '#');
+    $editBtn.textContent = 'Edit';
+    $deleteBtn.setAttribute('class', 'delete-btn');
+    $deleteBtn.setAttribute('href', '#');
+    $deleteBtn.textContent = 'Delete';
+    $plannerActions.setAttribute('class', 'planner-actions row space-evenly');
+    $plannerActions.appendChild($editBtn);
+    $plannerActions.appendChild($deleteBtn);
     newCell1.appendChild(document.createTextNode(item.time));
     newCell2.appendChild(document.createTextNode(item.notes));
-    newCell3.appendChild(document.createTextNode(item.notes));
+    newCell3.appendChild($plannerActions);
 }
 $form.addEventListener('submit', function (event) {
     // 1. collect the values from the form with the elements property
@@ -44,6 +53,11 @@ $form.addEventListener('submit', function (event) {
         day: $formElements.daysOfWeek.value,
         notes: $formElements.notesInput.value,
     };
+    eventsObject.eventsArr.push(item);
     renderResult(item);
+    $modal.close();
+    $form.reset();
+});
+$cancelbtn.addEventListener('click', function () {
     $modal.close();
 });
--- main.ts
+++ main.ts
@@ -10,43 +10,57 @@ interface FormElements extends HTMLFormControlsCollection {
   notesInput: HTMLTextAreaElement;
 }
 
+interface EventsObject {
+  eventsArr: Item[];
+  editing: null | Item;
+  nextEntryId: number;
+}
+
+const eventsObject: EventsObject = {
+  eventsArr: [],
+  editing: null,
+  nextEntryId: 1,
+};
+
 const $modal = document.querySelector('dialog') as HTMLDialogElement;
 const $addNewBtn = document.querySelector('#add-new') as HTMLAnchorElement;
 const $form = document.querySelector('#modal-form') as HTMLFormElement;
 const $table = document.querySelector('#events-table') as HTMLTableElement;
+const $cancelbtn = document.querySelector('#cancel') as HTMLAnchorElement;
 
 if (!$modal) throw new Error('no dialog found');
 if (!$addNewBtn) throw new Error('no add new button');
 if (!$form) throw new Error('no form');
 if (!$table) throw new Error('no table');
+if (!$cancelbtn) throw new Error('no cancel button found');
 
 $addNewBtn.addEventListener('click', () => {
   $modal.showModal();
 });
 
 function renderResult(item: Item): void {
-  /*
-  <tr>
-      <td>finalalla</td>
-      <td>finalalla</td>
-      <td>finalala</td>
-  </tr>
-
-  */
   const newRow = $table.insertRow(1);
   const newCell1 = newRow.insertCell(0);
   const newCell2 = newRow.insertCell(1);
   const newCell3 = newRow.insertCell(2);
 
-
   const $editBtn = document.createElement('a');
   const $deleteBtn = document.createElement('a');
+  const $plannerActions = document.createElement('div');
 
-  $editBtn.setAttribute('class', '')
+  $editBtn.setAttribute('class', 'edit-btn');
+  $editBtn.setAttribute('href', '#');
+  $editBtn.textContent = 'Edit';
+  $deleteBtn.setAttribute('class', 'delete-btn');
+  $deleteBtn.setAttribute('href', '#');
+  $deleteBtn.textContent = 'Delete';
+  $plannerActions.setAttribute('class', 'planner-actions row space-evenly');
+  $plannerActions.appendChild($editBtn);
+  $plannerActions.appendChild($deleteBtn);
 
   newCell1.appendChild(document.createTextNode(item.time));
   newCell2.appendChild(document.createTextNode(item.notes));
-  newCell3.appendChild(document.createTextNode(item.notes));
+  newCell3.appendChild($plannerActions);
 }
 
 $form.addEventListener('submit', (event: Event): void => {
@@ -63,6 +77,13 @@ $form.addEventListener('submit', (event: Event): void => {
     day: $formElements.daysOfWeek.value,
     notes: $formElements.notesInput.value,
   };
+
+  eventsObject.eventsArr.push(item);
   renderResult(item);
+  $modal.close();
+  $form.reset();
+});
+
+$cancelbtn.addEventListener('click', () => {
   $modal.close();
 });
